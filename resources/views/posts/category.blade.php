dbf87eb9db27912ed6866ecd4d89e81b10f24b4a
shotaaaaaaaaa <sgac31tvgam@gmail.com>
2023-02-24 10:31:40 +0900

    fix

--- resources/views/posts/category.blade.php
+++ resources/views/posts/category.blade.php
@@ -11,7 +11,6 @@
         <title>Category</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="public/styles.css" >
     </head>
     <body>
         <div class='categories'>
